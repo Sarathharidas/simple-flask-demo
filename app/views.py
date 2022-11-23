a178036de521af6a2e1dfd85b12a02c0ed9bcf8b
Sarathharidas <30758858+Sarathharidas@users.noreply.github.com>
2022-11-23 17:47:42 -0500

    Update views.py

--- app/views.py
+++ app/views.py
@@ -3,21 +3,28 @@ from app import app
 from flask import request, render_template
 
 
-@app.route("/")
-def index():
+@app.route('/')
+def hello_world():
+  return render_template("index.html")
 
-    """
-    This route will render a template.
-    If a query string comes into the URL, it will return a parsed
-    dictionary of the query string keys & values, using request.args
-    """
-
-    args = None
-
-    if request.args:
-
-        args = request.args
-
-        return render_template("public/index.html", args=args)
-
-    return render_template("public/index.html", args=args)
+@app.route('/translate',methods=["GET"])
+def downloadFile ():
+  #youtube_link =request.args.get('youtube-link')
+  #with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+      #ydl.download([youtube_link])
+  #os.system('whisper "test.wav" --language Hindi --task translate')
+  #text_array = []
+  #path_name = "test.wav.srt"
+  #path_to_download = os.path.abspath(path_name)
+  #print("Path",path_to_download)
+  #path_to_html = os.path.abspath("index.html")
+  #return render_template('index.html')
+  # return send_from_directory(os.getcwd(), "content/myfile.txt", as_attachment=True)
+  subtitles = open(path_name, "r").read().split("\n\n")
+  for i in subtitles:
+    text_array+=i.split("\n")
+  print(text_array)
+  return  render_template("index.html",text=text_array)
+    #For windows you need to use drive name [ex: F:/Example.pdf]
+    # path =
+    # return send_file(path, as_attachment=True)
